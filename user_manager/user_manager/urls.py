690d01b759e3861e342256ee3106fd83f8172662
Sebastian Ospino <sospino110@gmail.com>
2024-05-11 13:02:21 -0500

    v8.2

--- user_manager/user_manager/urls.py
+++ user_manager/user_manager/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-import views
+from . import views
 
 urlpatterns = [
     path('greet/<str:name>/', views.greet, name='greet'),
--- user_manager/user_manager/views.py
+++ user_manager/user_manager/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 
+@require_http_methods(["GET"])
 def greet(request, name):
     return HttpResponse(f"Hello, {name}!")
