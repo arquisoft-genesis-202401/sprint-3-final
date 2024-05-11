b2131b3286bdf6070bd3907eaa71a56c14bc7669
Sebastian Ospino <sospino110@gmail.com>
2024-05-11 13:26:36 -0500

    v8.6

--- user_manager/user_manager/services/cryptoManager.py
+++ user_manager/user_manager/services/cryptoManager.py
@@ -18,12 +18,22 @@ class CryptoManager:
         self.crypto_key_path = self.client.crypto_key_path(self.project_id, self.location_id, self.key_ring_id, self.crypto_key_id)
         self.hmac_key_path = self.client.crypto_key_path(self.project_id, self.location_id, self.key_ring_id, self.hmac_key_id)
 
-    def get_key_from_kms(self, key_path):
-        # TODO
-        return None
+    def get_key_from_kms(self, file_path):
+        # Request the key from KMS
+        key_version = self.client.crypto_key_version_path(
+            self.project_id, self.location_id, self.key_ring_id, self.crypto_key_id, '1')  # Assuming '1' is the key version
+        ciphertext = self.read_binary_file(file_path)
+        response = self.client.asymmetric_decrypt(
+            request={
+                "name": key_version,
+                "ciphertext": ciphertext  # The ciphertext would be your wrapped key
+            }
+        )
+        # The decrypted response contains the plaintext key
+        return response.plaintext
 
     def encrypt_data_aes(self, data):
-        key = self.get_key_from_kms(self.crypto_key_path)
+        key = self.get_key_from_kms("wrapped_aes_key.bin")
         backend = default_backend()
         cipher = Cipher(algorithms.AES(key), modes.CBC(self.iv), backend=backend)
         encryptor = cipher.encryptor()
@@ -33,7 +43,7 @@ class CryptoManager:
         return encrypted_data
 
     def decrypt_data_aes(self, encrypted_data):
-        key = self.get_key_from_kms(self.crypto_key_path)
+        key = self.get_key_from_kms("wrapped_aes_key.bin")
         backend = default_backend()
         cipher = Cipher(algorithms.AES(key), modes.CBC(self.iv), backend=backend)
         decryptor = cipher.decryptor()
@@ -43,10 +53,20 @@ class CryptoManager:
         return data
 
     def calculate_hmac(self, data):
-        key = self.get_key_from_kms(self.hmac_key_path)
+        key = self.get_key_from_kms("wrapped_hmac_key.bin")
         h = hmac.HMAC(key, hashes.SHA256(), backend=default_backend())
         h.update(data)
         return h.finalize()
+    
+    def read_binary_file(file_path):
+        try:
+            # Open the file in binary read mode
+            with open(file_path, 'rb') as file:
+                binary_data = file.read()
+                return binary_data
+        except IOError as e:
+            print(f"An error occurred while reading the file: {e}")
+            return None
 
 # Usage Example:
 crypto_manager = CryptoManager()
