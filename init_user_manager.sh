13a14b16e11d505d7ae691cf4d441ece45339524
Sebastian Ospino <sospino110@gmail.com>
2024-05-11 12:08:50 -0500

    v7

--- init_user_manager.sh
+++ init_user_manager.sh
@@ -5,7 +5,12 @@ echo 'export PORT_BUSINESS_DATABASE="${PORT_BUSINESS_DATABASE}"' >> ~/.profile
 echo 'export POSTGRESQL_DB_NAME="${POSTGRESQL_DB_NAME}"' >> ~/.profile
 echo 'export POSTGRESQL_DB_USER="${POSTGRESQL_DB_USER}"' >> ~/.profile
 echo 'export POSTGRESQL_DB_PASSWORD="${POSTGRESQL_DB_PASSWORD}"' >> ~/.profile
+echo 'export PROJECT_ID="${PROJECT_ID}"' >> ~/.profile
 echo 'export IV="${IV}"' >> ~/.profile
+echo 'export LOCATION_ID="${LOCATION_ID}"' >> ~/.profile
+echo 'export KEY_RING_ID="${KEY_RING_ID}"' >> ~/.profile
+echo 'export KEY_AES_ID="${KEY_AES_ID}"' >> ~/.profile
+echo 'export KEY_HMAC_ID="${KEY_HMAC_ID}"' >> ~/.profile
 source ~/.profile
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt update
