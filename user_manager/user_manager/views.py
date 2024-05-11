ad89fbac2331b35e1dded0f271e847cb923f9739
Sebastian Ospino <sospino110@gmail.com>
2024-05-11 13:06:19 -0500

    v8.3

--- user_manager/user_manager/views.py
+++ user_manager/user_manager/views.py
@@ -1,6 +1,9 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 
 @require_http_methods(["GET"])
 def greet(request, name):
-    return HttpResponse(f"Hello, {name}!")
+    response_data = {
+        'message': f'Hello, {name}!'
+    }
+    return JsonResponse(response_data)
