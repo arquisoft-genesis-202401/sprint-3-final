bc49b35ae061e51724b29ec192f4fc7c88eed868
Sebastian Ospino <sospino110@gmail.com>
2024-05-12 15:41:42 -0500

    v10.2

--- user_manager/user_manager/urls.py
+++ user_manager/user_manager/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import create_customer_application
-from .views import update_customer_application
-from .views import get_basic_information
+from django.views.decorators.csrf import csrf_exempt
+from .views import create_customer_application, update_customer_application, get_basic_information
 
 urlpatterns = [
-    path('create-application/', create_customer_application, name='create_application'),
-    path('update-application/<int:application_id>/', update_customer_application, name='update_application'),
-     path('get-basic-information/<int:application_id>/', get_basic_information, name='get_basic_information'),
+    path('create-application/', csrf_exempt(create_customer_application), name='create_application'),
+    path('update-application/<int:application_id>/', csrf_exempt(update_customer_application), name='update_application'),
+    path('get-basic-information/<int:application_id>/', csrf_exempt(get_basic_information), name='get_basic_information'),
 ]
