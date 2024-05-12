f644af52f25321d3c6f0df8aba4376e5f3c9cc65
Sebastian Ospino <sospino110@gmail.com>
2024-05-12 16:09:30 -0500

    v10.3

--- user_manager/user_manager/views.py
+++ user_manager/user_manager/views.py
@@ -3,24 +3,27 @@ from django.views.decorators.http import require_http_methods
 from .services.user_service import create_customer_application_basic_info
 from .services.user_service import update_customer_application_basic_info
 from .services.user_service import get_basic_information_by_application_id
+import traceback
+import sys
+import json
 
 @require_http_methods(['POST'])
 def create_customer_application(request):
     try:
         # Assuming JSON data is sent in request; validate as needed
-        data = request.POST
+        data = json.loads(request.body.decode('utf-8'))
 
         # Extract data from request
-        document_type = data.get('document_type')
-        document_number = data.get('document_number')
-        first_name = data.get('first_name')
-        last_name = data.get('last_name')
-        country = data.get('country')
-        state = data.get('state')
-        city = data.get('city')
-        address = data.get('address')
-        mobile_number = data.get('mobile_number')
-        email = data.get('email')
+        document_type = data['document_type']
+        document_number = data['document_number']
+        first_name = data['first_name']
+        last_name = data['last_name']
+        country = data['country']
+        state = data['state']
+        city = data['city']
+        address = data['address']
+        mobile_number = data['mobile_number']
+        email = data['email']
 
         # Check required fields
         if not all([document_type, document_number, first_name, last_name, country, state, city, address, mobile_number, email]):
@@ -35,13 +38,14 @@ def create_customer_application(request):
         return JsonResponse({'application_id': application_id})
 
     except Exception as e:
+        traceback.print_exc(file=sys.stdout)
         return HttpResponseBadRequest(f"An error occurred: {str(e)}")
     
 @require_http_methods(['POST'])
 def update_customer_application(request, application_id):
     try:
         # Assuming JSON data is sent in request; validate as needed
-        data = request.POST
+        data = request.body
 
         # Extract data from request
         first_name = data.get('first_name')
@@ -70,6 +74,7 @@ def update_customer_application(request, application_id):
         return JsonResponse({'application_id': result})
 
     except Exception as e:
+        traceback.print_exc(file=sys.stdout)
         return HttpResponseBadRequest(f"An error occurred: {str(e)}")
 
 @require_http_methods(['GET'])
@@ -86,4 +91,5 @@ def get_basic_information(request, application_id):
         return JsonResponse(result)
 
     except Exception as e:
+        traceback.print_exc(file=sys.stdout)
         return HttpResponseBadRequest(f"An error occurred: {str(e)}")
