444811a5634f4fdbc5c701fd81a272b6fce8e1f2
Sebastian Ospino <sospino110@gmail.com>
2024-05-13 02:03:41 -0500

    v16.1

--- user_manager/user_manager/urls.py
+++ user_manager/user_manager/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
-from .views import  send_otp_to_phone, create_customer_application, create_update_application_basic_info, get_basic_information
+from .views import  send_otp_to_phone, create_customer_application, create_update_application_basic_info, get_basic_information, get_customer_latest_application
 
 urlpatterns = [
     path('create-application/', csrf_exempt(create_customer_application), name='create_application'),
     path('update-application/<int:application_id>/', csrf_exempt(create_update_application_basic_info), name='update_application'),
     path('get-basic-information/<int:application_id>/', csrf_exempt(get_basic_information), name='get_basic_information'),
     path('send-otp/', csrf_exempt(send_otp_to_phone), name='send-otp'),
+    path('api/customer/latest-application/', get_customer_latest_application, name='get_customer_latest_application'),
 ]
